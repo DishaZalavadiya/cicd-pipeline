b6aa6367e967818848c41f4c50e0b59b9c7e3f0a
DishaZalavadiya <128746660+DishaZalavadiya@users.noreply.github.com>
2026-04-23 14:36:05 +0530

    Update HelloServlet.java

--- cicd-demo/src/main/java/com/myapp/HelloServlet.java
+++ cicd-demo/src/main/java/com/myapp/HelloServlet.java
@@ -1,9 +1,6 @@
-package com.myapp;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
